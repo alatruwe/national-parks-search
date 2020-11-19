132936bab4853ef934ea45a377ce5da7116a6af8
adelinel <adeline.latruwe@gmail.com>
2020-11-19 10:27:39 -0800

    added getResults() and buildQueryParams()

--- app.js
+++ app.js
@@ -15,40 +15,70 @@
 //API Key
 const apiKey = "nhbz6vHQiA3cxQvvtjnV8egSqABiX9snUwftUNi3";
 //API Base URL
-const apiUrl = 'https://developer.nps.gov/api/v1';
+const apiUrl = 'https://developer.nps.gov/api/v1/parks';
 
 function buildQueryParams(params) {
   //take object, for each key/value pair build a param/value pair
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   //return string with params
+  return queryItems.join('&');
 }
 
 function displayResults(responseJson) {
   //remove previous results
+  console.log(responseJson);
   //loop through responseJson
   //add to html ul each element with full name of the NP, description, website url, NP address
   //remove hidden class
 }
 
-function getResults(query, resultsLimit) {
-  console.log("Yay, getResults run!")
-  //const params = object with params needed
-  //call buildQueryParams(params) to get queryString
-  //build apiCall = apiUrl + queryString
+function getResults(stateCode, resultsLimit) {
+  //build header with apiKey
+  const options = {
+    headers: new Headers({
+      'X-Api-Key': apiKey
+    })
+  };
+  //params
+  const params = {
+    limit: resultsLimit,
+  };
+
+  //build and add statecodes to queryString (allows multiple states search)
+  const stateCodeList = stateCode.split(",");
+  let stateCodeQuery = '';
+  for (const property in stateCodeList) {
+    stateCodeQuery += `statecode=${stateCodeList[property]}` + '&';
+  }
+  let stateCodeQueryString = '&' + stateCodeQuery.slice(0, -1);
+
+  //build final api call
+  const queryString = buildQueryParams(params);
+  const apiCall = apiUrl + '?' + queryString + stateCodeQueryString;
+
   //call API
-  //display list to html, displayResults(responseJSon)
-  //handle errors
+  fetch(apiCall, options)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => {
+      $('.js-error-message').text(`An error occured: ${error.message}`);
+    });
 }
 
 function submitForm() {
   //watch for submit event
   $('form').submit(event => {
     event.preventDefault();
-    //get user entry area = stateCode param
+    //get user entry area = stateCode
     const stateCode = $('#area').val();
     //get user entry limit = resultsLimit
     const resultsLimit = $('#js-max-results').val();
-    console.log(stateCode);
-    console.log(resultsLimit);
     //get and display results, getResults(stateCode, resultsLimit)
     getResults(stateCode, resultsLimit);
   });
